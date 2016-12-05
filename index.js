c7ada3127a99b672880fca4184d194b9475c4dac
jonabasque <jonmultimedia@gmail.com>
2016-12-05 22:48:59 +0100

    Fixed bug ./lib calls to ./src from index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {default as Bars} from './lib/bars';
-import {default as Pie} from './lib/pie';
+import {default as Bars} from './src/bars';
+import {default as Pie} from './src/pie';
 
 export {Pie, Bars};
